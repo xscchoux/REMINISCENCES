235f3b02dd41b757a405019d6b2c2173bddec30c
Ed <ed78617@gmail.com>
2019-12-05 22:25:50 -0500

    set db to strict mode

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -86,6 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'E6770',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
         'USER': 'sc4400',
         'PASSWORD': 'Ccou4645#',
         'HOST': 'db-django.c1saaizp8z6b.us-east-1.rds.amazonaws.com',
