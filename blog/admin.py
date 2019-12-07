24113f039e0f8ea45882457bfca618e43244c85f
Ed <ed78617@gmail.com>
2019-12-06 21:42:43 -0500

    add upload page

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-
+admin.site.site_url = "/blog"
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = (
